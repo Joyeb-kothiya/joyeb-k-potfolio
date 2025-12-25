bf5d9e22626939e7d1d5c1cbfbb7842d6428f753
ziki-coder-29 <zikifukato37@gmail.com>
2025-12-25 17:18:25 +0530

    Fix Container typing and resolve build errors

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
+
+type ContainerProps = React.HTMLAttributes<HTMLDivElement>;
 
 export default function Container({
   children,
   className,
   ...props
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) {
+}: ContainerProps) {
   return (
     <div
-      className={`container mx-auto max-w-3xl px-4 animate-fade-in-blur ${className}`}
+      className={`container mx-auto max-w-3xl px-4 ${className ?? ""}`}
       {...props}
     >
       {children}
